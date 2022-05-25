93abe8feb91c9d34620cf9f140cca6fe17a5707f
Oppedal <nicholai.oj@gmail.com>
2022-05-25 13:10:06 +0200

    -

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 'use strict:';
+
 const markO = document.querySelector('.mark--o');
 const markX = document.querySelector('.mark--x');
 const gameBoard = (() => {
-  let emptyScore = new Array(9);
+  let emptyScore = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   let score = [`x`, , `o`, , `x`, , `x`, `o`];
   return {
     score,
     emptyScore,
   };
 })();
-const gameController = (() => {})();
+const gameController = (() => {
+  const boardDivs = document.querySelectorAll('.board');
+  boardDivs.forEach((button) => {
+    button.addEventListener('click', () => {
+      button.classList.add('.mark--o');
+      console.log(button.classList);
+    });
+  });
+  return boardDivs;
+})();
 
 const Player = (playerName, playerNumber) => {
   const getName = () => playerName;
