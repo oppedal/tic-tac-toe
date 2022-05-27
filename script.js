12c7e1be505898c5be4894dbac53fac99d348e14
Deerdevil <nicholai.oj@gmail.com>
2022-05-27 10:31:03 +0200

    Working on the gamelogics

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 'use strict:';
 
-const markO = document.querySelector('.mark--o');
-const markX = document.querySelector('.mark--x');
 const gameBoard = (() => {
-  let emptyScore = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let score = [`x`, , `o`, , `x`, , `x`, `o`];
+  const line_1 = [1, 2, 3];
+  const line_2 = [4, 5, 6];
+  const line_3 = [7, 8, 9];
+  let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   return {
-    score,
-    emptyScore,
+    board,
   };
 })();
+
 const gameController = (() => {
   const boardDivs = document.querySelectorAll('.board');
+
+  let activeMarkerClass = `mark--o`;
+  let activeMarker = `o`;
   boardDivs.forEach((button) => {
     button.addEventListener('click', () => {
-      button.classList.add('.mark--o');
+      button.classList.add(`${activeMarkerClass}`);
+
       console.log(button.classList);
     });
   });
-  return boardDivs;
+
+  return {
+    activeMarker,
+  };
 })();
+
+// const line_1 = [1, 2, 3];
+// line_1.forEach((item, i) => {
+//   if (item === 1) {
+//     line_1[i] = `${gameController.activeMarker}`;
+//   }
+// });
+// console.log(line_1);
 
 const Player = (playerName, playerNumber) => {
   const getName = () => playerName;
@@ -31,5 +47,3 @@ const Player = (playerName, playerNumber) => {
 
   return { getName, getMarker };
 };
-
-console.table(gameBoard.emptyScore.length);
