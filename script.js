68b2217a3ee8a1b0a3d113d6667fed9a2e8a57a3
Oppedal <nicholai.oj@gmail.com>
2022-05-31 21:53:14 +0200

    GEtting the js to work

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 'use strict:';
+const changePlayerBtn = document.querySelector('.changeplayer');
 
 const gameBoard = (() => {
-  const line_1 = [1, 2, 3];
-  const line_2 = [4, 5, 6];
-  const line_3 = [7, 8, 9];
-  let board;
+  let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const winning = {
+    win_o_1: ['o', 'o', 'o', 4, 5, 6, 7, 8, 9],
+    win_o_2: [1, 2, 3, 'o', 'o', 'o', 7, 8, 9],
+    win_o_3: [1, 2, 3, 4, 5, 6, 'o', 'o', 'o'],
+    win_o_4: ['o', 2, 3, 'o', 5, 6, 'o', 8, 9],
+    win_o_5: [1, 'o', 3, 4, 'o', 6, 7, 'o', 9],
+    win_o_6: [1, 2, 'o', 4, 5, 'o', 7, 8, 'o'],
+    win_o_7: ['o', 2, 3, 4, 'o', 6, 7, 8, 'o'],
+    win_o_8: [1, 2, 'o', 4, 'o', 6, 'o', 8, 9],
+    win_x_1: ['o', 'o', 'o', 4, 5, 6, 7, 8, 9],
+    win_x_2: [1, 2, 3, 'x', 'x', 'x', 7, 8, 9],
+    win_x_3: [1, 2, 3, 4, 5, 6, 'x', 'x', 'x'],
+    win_x_4: ['x', 2, 3, 'x', 5, 6, 'x', 8, 9],
+    win_x_5: [1, 'x', 3, 4, 'x', 6, 7, 'x', 9],
+    win_x_6: [1, 2, 'x', 4, 5, 'x', 7, 8, 'x'],
+    win_x_7: ['x', 2, 3, 4, 'x', 6, 7, 8, 'x'],
+    win_x_8: [1, 2, 'x', 4, 'x', 6, 'x', 8, 9],
+  };
 
   return {
-    boardTwo: [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-    ],
-    board: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    winning,
+    board,
   };
 })();
 const gameController = (() => {
@@ -20,8 +33,19 @@ const gameController = (() => {
   let activeMarker = `o`;
   let activeMarkerClass = `mark--${activeMarker}`;
   let numFromClass;
+  changePlayerBtn.addEventListener('click', () => {
+    if (activeMarker === 'o') {
+      activeMarker = 'x';
+      activeMarkerClass = `mark--${activeMarker}`;
+    } else {
+      activeMarker = 'o';
+      activeMarkerClass = `mark--${activeMarker}`;
+    }
+    console.log(activeMarker, activeMarkerClass);
+  });
   boardDivs.forEach((button) => {
     button.addEventListener('click', () => {
+      console.log(`Game board ${gameBoard.winning.win_o_1}`);
       button.classList.add(`${activeMarkerClass}`);
       //Get the last number from the boardDivs.//
       const getNumFromClass = button.classList[1];
@@ -35,32 +59,142 @@ const gameController = (() => {
       const equal = (a1, a2, ...indexes) =>
         indexes.every((i) => a1[i] === a2[i]);
 
-      let answer_1 = equal([...gameBoard.board], [...win_1], 0, 1, 2);
-      let answer_2 = equal([...gameBoard.board], [...win_2], 3, 4, 5);
-      let answer_3 = equal([...gameBoard.board], [...win_3], 6, 7, 8);
-      let answer_4 = equal([...gameBoard.board], [...win_4], 1, 4, 7);
-      let answer_5 = equal([...gameBoard.board], [...win_5], 2, 5, 8);
-      let answer_6 = equal([...gameBoard.board], [...win_6], 3, 6, 9);
-      let answer_7 = equal([...gameBoard.board], [...win_7], 1, 5, 9);
-      let answer_8 = equal([...gameBoard.board], [...win_8], 3, 5, 7);
+      let answer_o_1 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_o_1],
+        0,
+        1,
+        2
+      );
+      let answer_o_2 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_o_2],
+        3,
+        4,
+        5
+      );
+      let answer_o_3 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_o_3],
+        6,
+        7,
+        8
+      );
+      let answer_o_4 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_o_4],
+        0,
+        4,
+        6
+      );
+      let answer_o_5 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_o_5],
+        1,
+        4,
+        7
+      );
+      let answer_o_6 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_o_6],
+        2,
+        5,
+        8
+      );
+      let answer_o_7 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_o_7],
+        0,
+        4,
+        8
+      );
+      let answer_o_8 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_o_8],
+        2,
+        4,
+        6
+      );
+      let answer_x_1 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_x_1],
+        0,
+        1,
+        2
+      );
+      let answer_x_2 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_x_2],
+        3,
+        4,
+        5
+      );
+      let answer_x_3 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_x_3],
+        6,
+        7,
+        8
+      );
+      let answer_x_4 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_x_4],
+        0,
+        4,
+        6
+      );
+      let answer_x_5 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_x_5],
+        1,
+        4,
+        7
+      );
+      let answer_x_6 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_x_6],
+        2,
+        5,
+        8
+      );
+      let answer_x_7 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_x_7],
+        0,
+        4,
+        8
+      );
+      let answer_x_8 = equal(
+        [...gameBoard.board],
+        [...gameBoard.winning.win_x_8],
+        2,
+        4,
+        6
+      );
 
       const findWinner = {
-        answer_1,
-        answer_2,
-        answer_3,
-        // answer_4,
-        // answer_5,
-        answer_6,
-        // answer_7,
-        // answer_8,
+        answer_o_1,
+        answer_o_2,
+        answer_o_3,
+        answer_o_4,
+        answer_o_5,
+        answer_o_6,
+        answer_o_7,
+        answer_o_8,
+        answer_x_1,
+        answer_x_2,
+        answer_x_3,
+        answer_x_4,
+        answer_x_5,
+        answer_x_6,
+        answer_x_7,
+        answer_x_8,
       };
-      const checkWinner = Object.values(findWinner).some((val) => {
+      Object.values(findWinner).some((val) => {
         if (val === true) {
           console.log(`winner`);
         }
       });
-      // if (checkWinner) console.log(`Winner winner!`);
-      // console.log(gameBoard.board);
     });
   });
 
@@ -68,118 +202,9 @@ const gameController = (() => {
     activeMarker,
     boardDivs,
     numFromClass,
+    // checkWinner,
   };
 })();
-
-console.log(gameController.activeMarker);
-const win_1 = [
-  gameController.activeMarker,
-  gameController.activeMarker,
-  gameController.activeMarker,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-];
-const win_31 = [
-  gameController.activeMarker,
-  gameController.activeMarker,
-  gameController.activeMarker,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-];
-const win_2 = [
-  1,
-  2,
-  3,
-  gameController.activeMarker,
-  gameController.activeMarker,
-  gameController.activeMarker,
-  7,
-  8,
-  9,
-];
-const win_3 = [
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  gameController.activeMarker,
-  gameController.activeMarker,
-  gameController.activeMarker,
-];
-const win_4 = [
-  gameController.activeMarker,
-  2,
-  3,
-  gameController.activeMarker,
-  5,
-  6,
-  gameController.activeMarker,
-  8,
-  9,
-];
-
-const win_5 = [
-  1,
-  gameController.activeMarker,
-  3,
-  4,
-  gameController.activeMarker,
-  6,
-  7,
-  gameController.activeMarker,
-  9,
-];
-const win_6 = [
-  1,
-  2,
-  gameController.activeMarker,
-  4,
-  5,
-  gameController.activeMarker,
-  7,
-  8,
-  gameController.activeMarker,
-];
-const win_7 = [
-  gameController.activeMarker,
-  2,
-  3,
-  4,
-  gameController.activeMarker,
-  6,
-  7,
-  8,
-  gameController.activeMarker,
-];
-const win_8 = [
-  1,
-  2,
-  gameController.activeMarker,
-  4,
-  gameController.activeMarker,
-  6,
-  gameController.activeMarker,
-  8,
-  9,
-];
-
-// const line_1 = [1, 2, 3];
-// line_1.forEach((item, i) => {
-//   if (item === 1) {
-//     line_1[i] = `${gameController.activeMarker}`;
-//   }
-// });
-// console.log(line_1);
 
 const Player = (playerName, playerNumber) => {
   const getName = () => playerName;
